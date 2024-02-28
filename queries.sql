e8736fcd17d79d335089136894283178879e9f57
iscyr <109631477+iscyr@users.noreply.github.com>
2024-02-28 14:48:55 -0500

    Update and rename queries to queries.sql

--- queries.sql
+++ queries.sql
@@ -12,7 +12,14 @@ ORDER BY Freight DESC;
 -- 5. Create a report that shows all the CustomerID in lowercase letter and renamed as ID from the customers table.
 SELECT lower(CustomerID) as CustomerID from northwind.customers;
 -- 6. Create a report that shows the CompanyName, Fax, Phone, Country, HomePage from the suppliers table sorted by the Country in descending order then by CompanyName in ascending order.
+SELECT CompanyName, Fax, Phone, Country, HomePage FROM northwind.suppliers 
+ORDER BY Country DESC, CompanyName ASC;
 -- 7. Create a report that shows CompanyName, ContactName of all customers from ‘Buenos Aires' only.
+SELECT CompanyName, ContactName FROM northwind.customers WHERE City='Buenos Aires';
 -- 8. Create a report showing ProductName, UnitPrice, QuantityPerUnit of products that are out of stock.
+SELECT ProductName, UnitPrice, QuantityPerUnit FROM northwind.products WHERE UnitsInStock=0;
 -- 9. Create a report showing all the ContactName, Address, City of all customers not from Germany, Mexico, Spain.
+SELECT ContactName, Address, City FROM northwind.customers WHERE Country NOT IN ('Germany','Mexico','Spain');
 -- 10. Create a report showing OrderDate, ShippedDate, CustomerID, Freight of all orders placed on 21 May 1996.
+SELECT OrderDate, ShippedDate, CustomerID, Freight FROM northwind.orders
+WHERE OrderDate = '1996-05-21';
